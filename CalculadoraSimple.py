b5fd08217b896d484edd7ed5db036d6a623e940e
jesusroo <jesusroo04@gmail.com>
2026-06-10 18:12:23 -0400

    Create CalculadoraSimple.py

---
+++ CalculadoraSimple.py
@@ -0,0 +1,14 @@
+num1= int(input("ingresa el primer numero "))
+
+op= str(input("ingresa la operacion"))
+
+num2= int(input("ingresa el segundo numero "))
+
+if op=="+":
+   print("resultado: ",num1 + num2)
+elif op=="-":
+   print("resultado: ",num1 - num2)
+elif op=="*":
+   print("resultado: ",num1 * num2)
+elif op=="/":
+   print("resultado: ",num1 / num2)
